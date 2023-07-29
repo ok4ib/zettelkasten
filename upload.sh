3986f9265409e0f676219b88d258ca02606838b4
ok4ib <carmelo567@proton.me>
2023-07-29 23:05:43 +0200

    Commit automatico dallo script upload.sh

--- upload.sh
+++ upload.sh
@@ -1,11 +1,13 @@
 git add .
 git commit -m "Commit automatico dallo script upload.sh"
 git push
+git status
 cd ..
 mkdocs build
 cd site
 git add .
 git commit -m "Commit automatico dallo script upload.sh"
 git push
+git status
 
 
